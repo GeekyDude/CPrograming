13e87adb6c930eeb3f0e10c243b5767c711cac86
Harie Srinivasa <harie.srinivasa@gmail.com>
2017-04-26 22:45:00 -0700

    Adding more comments.

--- Variables/variable1.c
+++ Variables/variable1.c
@@ -6,33 +6,25 @@ int main() {
 
 	//int, double, float, char, long.
 
-	int a = 500;
+	int a = 500; //int - integer stores natural numbers
 	int b = 1000;
-
 	int c = a + b;
-
 	int d = 0;
+	int e = -500; //Can store negative numbers
 
-	int e = -500;
-
-	float f1 = 3.14;
+	float f1 = 3.14; //stores decimal numbers
 	float f2 = 22.0/7.0; // Pi
 
 	double f3 = (double)22 /(double) 7; // Pi
 
 	char c1 = 'h'; //0-9 a-z A-Z !@#$%^&
-
 	char name[100] = "Geeky Dude";
 
 
 	printf("Value of C is %d and Value of d is %d and Value of e is %d\n", c, d, e);
-
 	printf("Value of Pi in float is %f\n", f2);
-
 	printf("Value of Pi in double is %f\n", f3);
-
 	printf("Value of C1 is %c\n", c1);
-
 	printf("Hello %s\n", name);
 
 	return 0;
